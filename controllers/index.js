b115d90030bacaced893f2e525a2fa455b793e55
Anne Odom <afodom207@gmail.com>
2026-02-07 11:02:41 -0500

    MODIFY: Add route export code to make routes work

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,10 @@
 //index for controllers
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes.js");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+module.exports = router;
