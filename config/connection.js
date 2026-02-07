1110ae2a525d63ea8cf3256b74031651c6da08a6
Anne Odom <afodom207@gmail.com>
2026-02-07 11:00:42 -0500

    MODIFY: Insert connection information

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,21 @@
 //connection info
+const { Pool } = require("pg");
+require("dotenv").config();
+
+// Connect to database
+const db = process.env.DB_URL
+  ? new Pool({ connectionString: process.env.DB_URL })
+  : new Pool(
+      {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: "localhost",
+        database: process.env.DB_NAME,
+        port: 5432,
+      },
+      console.log(`Connected to the todolist_db database.`),
+    );
+
+db.connect();
+
+module.exports = db;
